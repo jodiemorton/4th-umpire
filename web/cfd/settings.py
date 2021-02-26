439c804572c915f57a5d4abca165c62436fa4942
jodiemorton <68940505+jodiemorton@users.noreply.github.com>
2021-02-26 20:43:54 +0530

    Update settings.py

--- web/cfd/settings.py
+++ web/cfd/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fourth_umpire',
+    'web.fourth_umpire',
 ]
 
 MIDDLEWARE = [
